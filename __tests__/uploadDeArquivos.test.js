c312949076c6d8376b0d64423459ca38e074e1e7
Domingos de Jesus Alcantara <45907626+DomingosAlcantara@users.noreply.github.com>
2023-05-11 21:14:56 -0300

    Ajustando os testes

--- __tests__/uploadDeArquivos.test.js
+++ __tests__/uploadDeArquivos.test.js
@@ -1,7 +1,7 @@
 const FolderBase = require("../src/Importacoes/uploadDeArquivos");
 
 describe("Testes de Importação dos Arquivos", () => {
-  test("Listando os Arquivos", async () => {
+  test("Testando o Array com os arquivos", async () => {
     const response = new FolderBase(
       "E:/Domingos/Documents/projetos-web/Domingos/RDATA/FEVEREIRO_22/Corrigidos/"
     );
